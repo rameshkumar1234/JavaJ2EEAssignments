cd392718f4c1674b1409c1198bd57f49ad3b6632
rames <rames@LAPTOP-ARMG3LKB>
2020-09-17 17:34:50 +0530

    HiberNate CRUD Operations

---
+++ HibernateCRUD/src/com/conroller/AddRestaurant.java
@@ -0,0 +1,49 @@
+package com.conroller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.DB;
+import com.model.Restaurant;
+
+
+@WebServlet({ "/AddRestaurant", "/Add" })
+public class AddRestaurant extends HttpServlet {
+	
+	
+	DB db;
+	
+	
+	@Override
+	public void init() throws ServletException {
+		db = new DB();
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Restaurant restaurant = new Restaurant();
+		restaurant.name = request.getParameter("txtName");
+		restaurant.phone = request.getParameter("txtPhone");
+		restaurant.address = request.getParameter("txtAddress");
+		restaurant.type = request.getParameter("txtType");
+		restaurant.ratings = Double.parseDouble(request.getParameter("txtRatings")); // Convert String to Double
+		restaurant.operatingHours = request.getParameter("txtHours");
+
+		String message = db.saveObject(restaurant);
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String messageToUser = "<html><body><center><h3>"+message+"</h3></center></body></html>";
+
+		out.print(messageToUser);
+		
+	}
+
+}
